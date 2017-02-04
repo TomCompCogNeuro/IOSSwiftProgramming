5041fbe14419a8f12bdd75f0fee80486d0943231
Thomas Carlson <thomas.shadowgrey@gmail.com>
2017-02-05 10:48:05 +1100

    My first IOS commit using sorcerer

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapcount = 0;
+    
+    @IBAction func PushButton2(sender: AnyObject) {
+        
+        theLabel.text = "Chicken Pants"
+        tapcount = tapcount + 1
+        print(tapcount)
+        
+        if tapcount > 10 {
+            theLabel.text = "You've pressed the button 10 times"
+        }
+        
+    }
+    @IBAction func ButtonTapped(sender: AnyObject) {
+        
+        theLabel.text = "Cheese Steak"
+        tapcount = tapcount + 1
+        print(tapcount)
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.redColor()
+        
     }
 
     override func didReceiveMemoryWarning() {
